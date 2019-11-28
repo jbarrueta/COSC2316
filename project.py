c92f473d5c24311bdfcdd9652b15fbe2c3d9806b
Jorge Barrueta <jbarrueta99@hotmail.com>
2019-11-27 18:49:26 -0600

    Update on project

--- project.py
+++ project.py
@@ -10,8 +10,8 @@
 # 2.1. Create function that reads a file and returns list of lines
 # TODO 3. when the program begins, read the text files that exist (users.txt, learn.txt)
 # 4. for each user in user.txt create a new instance of a Student. Save each into a users list.
-# TODO 5. save each translation into a dictionary - learnDict
-# TODO 6. save each key of the learnDict into a list
+# TODO 5. save each translation into a dictionary - learnDict MUST BE GLOBAL
+# TODO 6. start loop for menu until exit is selected
 # TODO 7. begin the menu, which includes Learn, Test, Leaderboard
 # TODO 8. if Learn is pressed, step though the learn dictionary and present the key. Once the user presses
 #         enter show the value, the next enter will go to the next key.
@@ -20,10 +20,13 @@
 #####     the equivalent to the value.
 ##### 11. if correct, the instance correct will go up as well as total, if wrong, wrong will go up one as well as total
 # TODO 12. Leaderboard will sort the list of users based on total correct, and display the top five.
-# TODO 13. Exit will end the program, saving the changes in users to the text file. (format: average correct wrong total name\n)
+# TODO 13. Exit ends program, save the changes in users to the text file. (format: average correct wrong total name\n)
+#          (ex: from the list users -> users[0].__str__() will write it in the correct format)
 
 
 ############# Python Code #############
+import random
+
 
 class Student:
     def __init__(self, name, correct=0, wrong=0, total=0):
@@ -36,8 +39,19 @@ class Student:
     def __str__(self):
         return self.average, self.correct, self.wrong, self.total, self.name
 
+    def tCorrect(self):
+        self.correct += 1
+        self.total += 1
+        self.average += 0 if self.total <= 0 else self.correct / self.total
 
-# Function Description: readFile function will read a file and save it into a list, if text file doesn't exits, it will create it
+    def tWrong(self):
+        self.wrong += 1
+        self.total += 1
+        self.average += 0 if self.total <= 0 else self.correct / self.total
+
+
+# Function Description: readFile function will read a file and save it into a list, if text file doesn't exits,
+#                       it will create it
 # Precondition: readFile will receive a text file
 # Postcondition: will return a list of file lines
 def readFile(txtFile):
@@ -55,31 +69,63 @@ def readFile(txtFile):
 # Precondition: must receive a list of users from the text file, a user by line
 # Postcondition: will return a list of Student instances
 def extractUsers(list1):
-    users = []
+    tempUsers = []
     count = 0
     for user in list1:
         splitUser = user.split()
         correctFormat = True
-        print(splitUser)
         try:
             avg = float(splitUser[0])
-            print(avg)
             correct = int(splitUser[1])
-            print(correct)
             wrong = int(splitUser[2])
-            print(wrong)
             total = int(splitUser[3])
-            print(total)
             name = " ".join(splitUser[4:])
-            users.append(Student(name.strip(), correct, wrong, total))
+            tempUsers.append(Student(name.strip(), correct, wrong, total))
         except ValueError:
             count += 1
     print("WARNING:", count, "could not be read and were skipped, please fix text file to be in the correct format")
-    return users
+    return tempUsers
+
+
+# Function Description: testOption will redirect student to either create a new user or open old user, then call testing
+# Precondition: will receive nothing
+# Postcondition: will return nothing, will make changes to student's progress
+def testOption():
+    usrInput = 0
+    while usrInput != 1 and usrInput != 2:
+        usrInput = int(input('''1. Create new user
+        2. Open old user
+        >>> '''))
+    if usrInput == 1:
+        name = input("Please enter your name: ")
+        users.append((Student(name)))
+        n = -1
+    elif usrInput == 2:
+        name = input("Please enter your name (enter exactly as you did before): ")
+        for index in range(users.length):
+            if users[index].name == name.strip():
+                n = index
+    testing(n)
+
+
+# Function Description: testing will carry out the actual test and make the changes to Student
+# Precondition: will receive the index of the current student testing
+# Postcondition: will return nothing
+def testing(i):
+    randomKeys = random.sample(list(learnDict),5)
+    for key in randomKeys:
+        usrAns = input("Write the Spanish translation.\n" + key + ": ")
+        if usrAns == learnDict[key]:
+            users[i].tCorrect()
+        else:
+            users[i].tWrong()
+
 
 
 # Driver Program
-usersList = readFile("users.txt")
-print(usersList)
-users = extractUsers(usersList)
-# print(users[1].__str__())
+usersFile = readFile("users.txt")
+print(usersFile)
+users = extractUsers(usersFile)
+learnDict = {"hello":"hola", "house":"casa","rock":"piedra","room":"piedra","telephone":"telefono","computer":"computer"}
+
+testOption()
